03153dbe4b443b0e5c6dc029057be5e87ee445ef
chandlercraig <hellochandlercraig@gmail.com>
2021-01-04 21:27:58 -0500

    sorted watir load

--- lib/fresh_astrologer/cli.rb
+++ lib/fresh_astrologer/cli.rb
@@ -1,4 +1,4 @@
-class FreshAstrologer::CLI
+class CLI
 
   def call
     puts "Welcome to Fresh Astrologer!"
--- lib/fresh_astrologer/scraper.rb
+++ lib/fresh_astrologer/scraper.rb
@@ -1,10 +1,13 @@
+require 'watir'
+require 'nokogiri'
+
 class Scraper
   def scrape_fresh_in_theaters
     browser = Watir::Browser.new
     browser.goto("https://www.rottentomatoes.com/browse/cf-in-theaters")
     js_doc = browser.element(css: ".mb-movie").wait_until(&:present?)
     news = Nokogiri::HTML(js_doc.inner_html)
-    desired_result = doc.css('.mb-movie')
+    desired_result = news('.mb-movie')
     # returns array of movies
     binding.pry
   end
