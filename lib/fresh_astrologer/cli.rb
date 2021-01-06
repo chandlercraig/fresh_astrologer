98db259e023263f9515f510beec961634190da57
chandlercraig <hellochandlercraig@gmail.com>
2021-01-05 21:24:58 -0500

    updated cli proceedure

--- lib/fresh_astrologer/cli.rb
+++ lib/fresh_astrologer/cli.rb
@@ -2,34 +2,46 @@ class CLI
 
   def call
     puts "Welcome to Fresh Astrologer, a Rotten Tomatoes™ trivia game for the new-age film buff!"
-    get_movie_titles
-    list_titles
+    puts "----------"
+    puts "These are the freshest movies in theaters:" 
+    list_movie_titles
+    puts "----------"
     get_user_movie
-
+    #get_director_info
   end
 
-  def get_movie_titles
-    # @movies = use Scraper.scrape_fresh_in_theaters in a Movies class to get titles array
-  end
-
-  def list_titles
-    # @movies.each_with_index(1) {|movie, index| puts "#{index}. #{movie}"}
+  def list_movie_titles
+    @@movies = Scraper.new.scrape_fresh_in_theaters
+    @@movies.each_with_index {|movie, index| puts "#{index+1}. #{movie}"}
   end
 
   def get_user_movie
-    puts "These are the freshest movies in theaters:" 
-    Scraper.new.scrape_fresh_in_theaters
     puts "Which movie interests you?"
-    # chosen_movie = gets.strip
-    # if @movies.include? {chosen_movie}
-    #   return chosen_movie
-    # else
-    #   puts "invalid entry"
-    #   gets_user_movie method restarts
-    # end
+    @@chosen_movie = gets.strip
+    @@chosen_movie
+    # add functionality for entering index number as movie selection?
+    if @@movies.include? (@@chosen_movie)
+      get_director_info
+    else
+      puts "invalid entry"
+      get_user_movie
+    end
   end
 
   def get_director_info
+    puts "----------"
+    puts "You've chosen #{@@chosen_movie}. Great choice!"
+    puts "Would you like to guess the director? (enter y or n)"
+    @chosen_dir = gets.strip
+    @chosen_dir
+    if @chosen_dir == "y"
+      puts "Enter directors name below"
+    elsif @chosen_dir == "n"
+      puts "No shame in that game! The director of #{@@chosen_movie} is..."
+    else
+      puts "invalid entry"
+      get_director_info
+    end
     # use scrape_wiki to get director name and birthday, then hardcode star sign
     # do all of this in a Director class, then pipe into this method
   end
--- lib/fresh_astrologer/scraper.rb
+++ lib/fresh_astrologer/scraper.rb
@@ -2,20 +2,22 @@ require 'watir'
 require 'nokogiri'
 
 class Scraper
-  
+
   def scrape_fresh_in_theaters
     fresh_titles = []
     browser = Watir::Browser.new
     browser.goto("https://www.rottentomatoes.com/browse/cf-in-theaters")
     js_doc = browser.element(css: ".mb-movies").wait_until(&:present?)
-    news = Nokogiri::HTML(js_doc.inner_html)
-    news.css(".mb-movie .movieTitle").each do |movie|   
-    fresh_titles << movie.text
-    end
-    fresh_titles.each_with_index {|movi, index| puts "#{index+1}. #{movi}"}
+    mov_el = Nokogiri::HTML(js_doc.inner_html)
+    browser.close
+    mov_el.css(".mb-movie .movieTitle").each {|mov| fresh_titles << mov.text}
+    fresh_titles
   end
 
   def scrape_wiki
+    search_url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
+    mov_4_html = search_url + "Farewell_Amor"
+
     # scrape for director name and birthday
 
   end
